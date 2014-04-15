a3146248044662a2a0128e0f1ab589466c8dbbad
Anna Yoney <ayoney@rockefeller.edu>
2014-04-15 13:28:22 -0400

    Make compatible with  DeltaVision files

---
+++ Cytoo/runTileLoop.m
@@ -0,0 +1,14 @@
+function runTileLoop(direc,chans,imgsperprocessor,nloop,maxims,bIms,nIms,paramfile)
+
+s=matlabpool('size');
+if s > 0
+    matlabpool close;
+end
+matlabpool('local',12);
+parfor ii=1:nloop
+    n1=(ii-1)*imgsperprocessor+1;
+    n2=min(ii*imgsperprocessor,maxims);
+    outfile=[direc filesep 'out_' int2str(n1) '.mat'];
+    runTile(direc,outfile,chans,[n1 n2],bIms,nIms,paramfile);
+end
+matlabpool close;
