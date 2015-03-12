8ffc42992c427fd231c2ef2f1d9fdc59a81a48f1
aryeh <warmflasha@gmail.com>
2015-03-12 12:11:14 -0500

    added ability to submit MM jobs on cluster

---
+++ Cytoo/runTileLoopMMCluster.m
@@ -0,0 +1,20 @@
+function runTileLoopMMCluster(direc,paramfile)
+
+files = readMMdirectory(direc);
+
+nImages = length(files.pos_x)*length(files.pos_y);
+
+for ii=1:nImages
+    scriptfile = [direc filesep '.jobscripts' filesep 'job_' int2str(ii) '.pbs'];
+    fid = fopen(scriptfile,'w');
+    fprintf(fid,['#PBS -N job_' int2str(ii) ' \n']); 
+    fprintf(fid,'#PBS -q serial\n');
+    fprintf(fid,'#PBS -l nodes=1:ppn=1,walltime=00:10:00\n');
+    fprintf(fid,'#PBS -W x=NACCESSPOLICY:SINGLEJOB\n');
+    fprintf(fid,'#PBS -M warmflasha@gmail.com\n');
+    fprintf(fid,'#PBS -V\n');
+    fprintf(fid,'#PBS -m abe\n');    
+    fprintf(fid,['matlab -nodisplay -r "runTileMMCluster(''' direc ''',outall.mat,' int2str(ii) ',''' paramfile '''); quit"']);
+    fclose(fid);
+    system(['qsub ./' scriptfile]);
+end
---
+++ Cytoo/runTileMMCluster.m
@@ -0,0 +1,66 @@
+function runTileMMCluster(direc,matfile,pos,paramfile)
+
+global userParam;
+
+try
+    eval(paramfile);
+catch
+    error('Error evaluating paramfile.');
+end
+
+files=readMMdirectory(direc);
+
+pp=load([direc filesep matfile],'bIms','nIms');
+bIms = pp.bIms; nIms = pp.nIms;
+
+nImages=length(files.chan)-1;
+
+xmax = max(files.pos_x)+1;
+ymax = max(files.pos_y)+1;
+
+
+%read nuclear image, smooth and background subtract
+
+[x, y]=ind2sub([xmax ymax],pos);
+f1nm = mkMMfilename(files,x-1,y-1,[],[],1);
+
+imgfiles.nucfile=f1nm{1};
+nuc=imread(f1nm{1});
+si=size(nuc);
+%apply gaussian smoothing
+nuc=smoothImage(nuc,userParam.gaussRadius,userParam.gaussSigma);
+%subtract precalculated background Image
+nuc=imsubtract(nuc,bIms{1});
+nuc=immultiply(im2double(nuc),nIms{1});
+nuc=uint16(65536*nuc);
+
+
+fimg=zeros(si(1),si(2),nImages);
+for jj=2:(nImages+1)
+    f1nm = mkMMfilename(files,x-1,y-1,[],[],jj);
+    fimgnow=imread(f1nm{1});
+    fimgnow = smoothImage(fimgnow,userParam.gaussRadius,userParam.gaussSigma);
+    imgfiles.smadfile{jj-1}=f1nm{1};
+    fimgnow=imsubtract(fimgnow,bIms{jj});
+    fimgnow=immultiply(im2double(fimgnow),nIms{jj});
+    fimg(:,:,jj-1)=uint16(65536*fimgnow);
+end
+
+
+
+%Initialize error string
+userParam.errorStr=sprintf('Position %d\n',pos);
+
+[maskC, statsN]=segmentCells2(nuc,fimg);
+[~, statsN]=addCellAvr2Stats(maskC,fimg,statsN);
+
+if ~isempty(statsN)
+    outdat=outputData4AWTracker(statsN,nuc,nImages);
+    imgfiles.errorStr=userParam.errorStr;
+    % compress and save the binary mask for nuclei
+    imgfiles.compressNucMask = compressBinaryImg([statsN.PixelIdxList], size(nuc));
+    outfile = [direc filesep '.tmp_analysis' filesep 'out_' int2str(pos) '.mat'];
+    save(outfile,'outdat','userParam','imgfiles');
+end
+
+
