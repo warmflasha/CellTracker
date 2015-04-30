4d060493c41e00c38fcb75ed13e2afbfad0dbde4
aryeh <warmflasha@gmail.com>
2015-04-30 13:10:44 -0500

    Fixed bug that prevented returning data from mkFullCytooPlotPeaks

--- Cytoo/mkFullCytooPlotPeaks.m
+++ Cytoo/mkFullCytooPlotPeaks.m
@@ -9,14 +9,16 @@ peaks=pp.peaks;
 ac=pp.acoords;
 
 cc=colorcube(19);
+q=1;
 figure; hold on;
 for ii=1:length(peaks)
     if ~isempty(peaks{ii})
     toadd=[ac(ii).absinds(2) ac(ii).absinds(1)];
     dtoplot=bsxfun(@plus,peaks{ii}(:,1:2),toadd);
+    ncells = size(peaks{ii},1);
     if returndat
-        alldat(q:(q+col(ii).ncells-1),:)=[dtoplot col(ii).data(:,3:end)];
-        q=q+col(ii).ncells;
+        alldat(q:(q+ncells-1),:)=[dtoplot peaks{ii}(:,3:end)];
+        q=q+ncells;
     end
     plot(dtoplot(:,2),dtoplot(:,1),'.','Color',cc(mod(ii,19)+1,:));
     text(toadd(1),toadd(2),int2str(ii),'Color','m');
