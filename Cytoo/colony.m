f4140bd133e3f2371508e408a224266f0429b8d7
aryeh <warmflasha@gmail.com>
2015-04-30 13:05:32 -0500

    Fixed colony method assembleColonyMM

--- Cytoo/colony.m
+++ Cytoo/colony.m
@@ -228,7 +228,7 @@ classdef colony
             
         end
         
-        function fullImage=assembleColonyMM(obj,direc,colnum,backIm,normIm)
+        function fullImage=assembleColonyMM(obj,direc,acoords,si,backIm,normIm)
             %Assemble the image of the colony.
             %
             %fullImage=assembleColony(obj,direc,imKeyWord,overlayPoints,backIm)
@@ -242,82 +242,40 @@ classdef colony
             %cell array of the same length as imKeyword
             
             
-            % disp('Calling this function!!');
             
             
             ff=readMMdirectory(direc);
             dims = [ max(ff.pos_x)+1 max(ff.pos_y)+1];
-            %wavenames=ff.chan;
-            %imname = mkMMfilename(ff,pos_x,pos_y,[],[],wavenames(jj));
-            % coldata=obj(colnum).data;
-            
-            imnums=obj(colnum).imagenumbers;
-            dim1=find(diff(imnums)>1,1,'first');%dim1=find(diff(obj.imagenumbers)>1,1,'first');
-            if isempty(dim1)
-                total1=length(imnums);
-                total2=1;
-            else
-                total1=dim1;
-                total2=length(imnums)/dim1;
-            end
-            
-            ac=obj.imagecoords;
             
             
-            %[junk, imFiles]=folderFilesFromKeyword(direc,imKeyWord{1});
+            imnums=obj.imagenumbers;
             [pos_x, pos_y]=ind2sub(dims,imnums);
-            %direc = [files.direc filesep files.prefix 'Pos_' pos_x '_' pos_y];
-            imnameCell = mkMMfilename(ff,pos_x-1,pos_y-1,[],[],ff.chan);
             
-            if isempty(imnameCell)
-                error('Error: files with first keyword not found...');
-            end
-            
-            tmp1=imread([imnameCell{1}]);% AN imFiles(1).name
-            si=size(tmp1);
-            %si=[2048 2048];
+            %             tosub=[ac.absinds];
+            %             tosub=reshape(tosub,2,length(ac));
+            %             tosub=min(tosub,[],2)';
             
             for jj=1:length(ff.chan)
-                %fullImage{jj}=zeros(si(1)*max(coords(:,1)),si(2)*max(coords(:,2)));
-                fullImage{jj}=uint16(zeros(si(1)*total1,si(2)*total2));
+                fullImage{jj}=uint16(zeros(si(1)*length(unique(pos_x)),si(2)*length(unique(pos_y))));
+            end
+            
+            for ii=1:length(imnums)
+                %currinds = ac(ii).absinds-tosub+1;
                 
-                for ii=1:length(imnums)
-                    %[pos_x pos_y]=ind2sub(dims,imnums(ii));
-                    % imname = mkMMfilename(ff,pos_x,pos_y,[],[],ff.chan);
+                xnow = pos_x(ii); ynow=pos_y(ii);
+                currinds=[(max(pos_x)-xnow)*si(1)+1 (ynow-min(pos_y))*si(2)+1];
+                for kk=(min(pos_y)+1):ynow
+                    currinds(2)=currinds(2)-acoords(sub2ind(dims,xnow,kk)).wabove(1);
+                end
+                for mm= xnow:(max(pos_x)-1)
+                    currinds(1)=currinds(1)-acoords(sub2ind(dims,mm,ynow)).wside(1);
+                end
+                currinds=currinds([2 1]); %somehow the above ended up backwar
+                
+                filenames = mkMMfilename(ff,pos_x(ii)-1,pos_y(ii)-1,[],[],ff.chan);
+                for jj=1:length(filenames)
+                    currimg=imread(filenames{jj});
                     
-                    if isempty(dim1)
-                        over2=0;
-                        over1=ii-1;
-                    else
-                        over2=fix((ii-1)/dim1);
-                        over1=rem((ii-1),dim1);
-                        
-                    end
-                    
-                    
-                    
-                    currinds=[over1*si(1)+1 over2*si(2)+1];
-                    for kk=1:over1
-                        currinds(1)=currinds(1)-ac(ii-(kk-1)).wabove(1);
-                    end
-                    for mm=1:over2
-                        currinds(2)=currinds(2)-ac(ii-(mm-1)*dim1).wside(1);
-                    end
-                    
-                    imname=imnameCell{jj};%ff.chan{ii};
-                    
-                    
-                    %If file doesn't exist, return empty array for this
-                    %keyword.
-                    if ~exist(imname,'file')
-                        disp(['Warning: file ' imname ' not found. Skipping this keyword.']);
-                        fullImage{jj}=[];
-                        break;
-                    end
-                    %disp(imname);
-                    currimg=imread(imname);
-                    
-                    %background subtraction
                     if exist('backIm','var')
                         currimg=imsubtract(currimg,backIm{jj});
                     end
@@ -329,10 +287,9 @@ classdef colony
                     end
                     
                     fullImage{jj}(currinds(1):(currinds(1)+si(1)-1),currinds(2):(currinds(2)+si(2)-1))=newIm;
-                    
                 end
-                
             end
+            
             
             
         end
@@ -563,7 +520,7 @@ for ii=1:length(imnums)
     newdata(currimgcells,:)=origdata;
     
 end
-total1 = max(ximg)-min(ximg)+1; 
+total1 = max(ximg)-min(ximg)+1;
 total2 = max(yimg)-min(yimg)+1;
 compressednuc=compressBinaryImg(sub2ind([total2*si(1) total1*si(2)],nucpixall(:,1),nucpixall(:,2)),[total1*si(1) total2*si(2)]);
 end
