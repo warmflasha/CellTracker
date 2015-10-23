dd48e72d51dc98f9f68f7ddb4b41c591faed7547
aryeh <warmflasha@gmail.com>
2015-10-22 23:04:21 -0500

    more cluster updates

--- Cytoo/runTileLoopMMCluster.m
+++ Cytoo/runTileLoopMMCluster.m
@@ -13,13 +13,13 @@ for ii=1:nloop
     fid = fopen(scriptfile,'w');
     fprintf(fid,'#!/bin/bash\n')
     fprintf(fid,['#SBATCH --job-name job_' int2str(ii) ' \n']); 
-    fprintf(fid,'#SBATCH --partition=serial\n');
+    fprintf(fid,'#SBATCH --partition=commons\n');
     fprintf(fid,'#SBATCH --ntasks=1\n');
     fprintf(fid,'#SBATCH --mem-per-cpu=1000m\n');
     fprintf(fid,'#SBATCH --time=00:30:00\n');
     n1=(ii-1)*imgsperprocessor+1;
     n2=min(ii*imgsperprocessor,nImages);
-    fprintf(fid,['srun matlab -nodisplay -r "addpath(genpath(''/home/aw21/CellTracker'')); parpool(''local'',' int2str(imgsperprocessor) '); runTileMMCluster(''' direc ''',''outall.mat'',' int2str(n1) ':' int2str(n2)  ',''' paramfile '''); delete(gcp); quit"']);
+    fprintf(fid,['srun matlab -nodisplay -r "addpath(genpath(''/home/aw21/CellTracker'')); parpool(' int2str(imgsperprocessor) '); runTileMMCluster(''' direc ''',''outall.mat'',' int2str(n1) ':' int2str(n2)  ',''' paramfile '''); delete(gcp); quit"']);
     fclose(fid);
     system(['sbatch ' scriptfile]);
 end
