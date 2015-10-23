65846ed53860a6a9fda1ede1434f5231f26d3f1b
aryeh <warmflasha@gmail.com>
2015-10-22 21:12:53 -0500

    update cluster code for SLURM

--- Cytoo/runTileLoopMMCluster.m
+++ Cytoo/runTileLoopMMCluster.m
@@ -11,15 +11,15 @@ nloop = ceil(nImages/imgsperprocessor);
 for ii=1:nloop
     scriptfile = [direc filesep '.jobscripts' filesep 'job_' int2str(ii) '.pbs'];
     fid = fopen(scriptfile,'w');
-    fprintf(fid,['#PBS -N job_' int2str(ii) ' \n']); 
-    fprintf(fid,'#PBS -q serial\n');
-    fprintf(fid,'#PBS -l nodes=1:ppn=1,walltime=00:10:00\n');
-    fprintf(fid,'#PBS -W x=NACCESSPOLICY:SINGLEJOB\n');
-    fprintf(fid,'#PBS -V\n');
-    fprintf(fid,'#PBS -m abe\n');  
+    fprintf(fid,'#!/bin/bash')
+    fprintf(fid,['#SBATCH --job-name job_' int2str(ii) ' \n']); 
+    fprintf(fid,'#SBATCH --partition= serial\n');
+    fprintf(fid,'#SBATCH --ntasks=1\n');
+    fprintf(fid,'#SBATCH --mem-per-cpu=1000m\n');
+    fprintf(fid,'#SBATCH --time=00:30:00\n');
     n1=(ii-1)*imgsperprocessor+1;
     n2=min(ii*imgsperprocessor,nImages);
-    fprintf(fid,['matlab -nodisplay -r "addpath(genpath(''/home/aw21/CellTracker'')); matlabpool(''local'',' int2str(imgsperprocessor) '); runTileMMCluster(''' direc ''',''outall.mat'',' int2str(n1) ':' int2str(n2)  ',''' paramfile '''); matlabpool(''close''); quit"']);
+    fprintf(fid,['srun matlab -nodisplay -r "addpath(genpath(''/home/aw21/CellTracker'')); matlabpool(''local'',' int2str(imgsperprocessor) '); runTileMMCluster(''' direc ''',''outall.mat'',' int2str(n1) ':' int2str(n2)  ',''' paramfile '''); matlabpool(''close''); quit"']);
     fclose(fid);
-    system(['qsub ' scriptfile]);
+    system(['sbatch ' scriptfile]);
 end
