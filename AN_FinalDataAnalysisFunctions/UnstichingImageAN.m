fe78c80326f913bc51768cf2eb24baa54772f920
Anastasiia <nmnemashkalo@gmail.com>
2015-11-23 15:09:10 -0600

    useless

--- AN_FinalDataAnalysisFunctions/UnstichingImageAN.m
+++ AN_FinalDataAnalysisFunctions/UnstichingImageAN.m
@@ -8,14 +8,16 @@ s2 = size(I,2);
 
 mag = [2048,2048]; % also input parameter
 
-dim2 = s1/mag; %rows, in the y direction
+dim2 = s1/mag(2); %rows, in the y direction
 dim2 = round(s1/mag(2));
 
-dim1 = s2/mag; % cols, in the x direction
+dim1 = s2/mag(1); % cols, in the x direction
 dim1 = round(s2/mag(1));
 
-for i = 1:dim1         % here need to set up correctly how the matrix will be subdevided into images (j and i ) and also code them into the name of the resulting position
-    for j = 1:dim2
-pos0_0 = I(1:2048,1:2048);
-    end
+for j = 1:dim1         % here need to set up correctly how the matrix will be subdevided into images (j and i ) and also code them into the name of the resulting position
+    for k = 1 % only look at the first column of images
+     
+pos{j} = I(2048*(j-1):(2048*(j)),(2048)*(k-1):(2048*(k)));
+           
 end
+%save(['CellTraces_' num2str(N) ],'datcell');
