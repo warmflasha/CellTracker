72eb8ba355650c14f511ebcfc70e4ea617b10688
Anastasiia <nmnemashkalo@gmail.com>
2015-12-22 11:34:02 -0600

    bug in trajectory when adding colszdata if there was merge

--- paramFiles/newTrackParam.m
+++ paramFiles/newTrackParam.m
@@ -2,11 +2,11 @@ function newTrackParam
 
 global userParam;
 
-userParam.L = 70;
+userParam.L = 80;
 userParam.sizeImg = [2048, 2048];
 userParam.verboseCellTrackerEDS = 0;
 userParam.minTrajLen = 4;
-userParam.mergeGap = 4;
+userParam.mergeGap = 2;
 userParam.sclDstCost = [1 2];
 userParam.minlength = 20;
 userParam.mincyto = 0;
