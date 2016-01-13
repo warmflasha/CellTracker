a598001f450222ee643f7065c663b52e4c5ff85b
Anastasiia <nmnemashkalo@gmail.com>
2016-01-12 19:37:12 -0600

    better params

--- paramFiles/newTrackParam.m
+++ paramFiles/newTrackParam.m
@@ -6,12 +6,12 @@ global userParam;
 % userParam.L = 80;
 % userParam.sizeImg = [2048, 2048];
 % =======
-userParam.L = 70;
+userParam.L = 80;
 userParam.sizeImg = [1024, 1024];
 
 userParam.verboseCellTrackerEDS = 0;
 userParam.minTrajLen = 10;%4
-userParam.mergeGap = 10;%2
+userParam.mergeGap = 5;%2
 userParam.sclDstCost = [1 2];
 userParam.minlength = 20;
 userParam.mincyto = 0;
