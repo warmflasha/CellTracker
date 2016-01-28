6a1ed3f5040737d42550754493a4f831cb728852
Anastasiia <nmnemashkalo@gmail.com>
2016-01-27 19:08:30 -0600

    important correction

--- DataClasses/dynColony.m
+++ DataClasses/dynColony.m
@@ -48,7 +48,7 @@ classdef dynColony %object for storing dynamic colony level data
                 tmp = (obj.cells(k).onframes)';
                 data_perframe((tmp(1):tmp(end)),k) = one;
                 dynsmad(k,1) = mean(nonzeros(data_perframe(1:fr_stim,k)));
-                dynsmad(k,2) = mean(nonzeros(data_perframe((fr_stim+jumptime):(fr_stim+jumptime+resptime),k)));
+                dynsmad(k,2) = mean(nonzeros(data_perframe((fr_stim+jumptime):(resptime),k)));%fr_stim+jumptime+
                 dynsmad(k,3) = mean(nonzeros(data_perframe(range(1):range(2),k)));
                 dynsmad(k,4) = abs(dynsmad(k,1)-mean(nonzeros(data_perframe((fr_stim+jumptime):(fr_stim+jumptime+range(2)),k))));
             end
