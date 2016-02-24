14ac84c30d725a05834f7d8fad14a6bb0a2240cc
Aryeh <warmflasha@gmail.com>
2016-02-24 10:37:03 -0600

    add bitdepth to bfMaxIntensity

--- Utilities/bfMaxIntensity.m
+++ Utilities/bfMaxIntensity.m
@@ -1,4 +1,4 @@
-function max_img = bfMaxIntensity(reader,time,chan)
+function max_img = bfMaxIntensity(reader,time,chan,bitdepth)
 
 
 nz=reader.getSizeZ;
@@ -7,8 +7,20 @@ nz=reader.getSizeZ;
 for ii=1:nz
     iPlane=reader.getIndex(ii - 1, chan -1, time - 1) + 1;
     img_now=bfGetPlane(reader,iPlane);
+    
+    if exist('bitdepth','var')
+        switch bitdepth
+            case 8
+                img_now = uint8(img_now);
+            case 16
+                img_now = uint16(img_now);
+            otherwise
+                disp('bitdepth must be 8 or 16');
+        end
+                
+    end
     if ii == 1
-        max_img = img_now;
+        max_img =  img_now;
     else
         max_img = max(max_img,img_now);
     end
