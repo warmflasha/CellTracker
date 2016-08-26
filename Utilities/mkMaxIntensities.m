f14290248056b9e7be23158c5bf96f67a905c4bf
Joseph Massey <jm48@rice.edu>
2016-08-26 13:36:59 -0500

    QuotientError.m, some patameter files, mkMaxIntensities

--- Utilities/mkMaxIntensities.m
+++ Utilities/mkMaxIntensities.m
@@ -1,12 +1,20 @@
-function [ output_args ] = mkMaxIntensities( direc )
-% Makes Max Intensity Projections for all .tif files in a directory
-%   Outputs projections to direc/MaxIntensities
+function [ output_args ] = mkMaxIntensities( direc,extension )
+% Makes Max Intensity Projections for all files in a directory  which have
+% a given file extension. Output files will be .tiff under
+% direc/MaxIntensities
+%
+%   
+
+% example:
+%   mkMaxIntensities(experiment,'.tif') results in a new 
 
 
-mkdir('MaxIntensity');
-imglist = dir(fullfile(direc, '*.tif'));
+mkdir([direc filesep 'MaxIntensity']);
+imglist = dir(fullfile(direc, ['*' extension]));
 parfor ii = 1:length(imglist)
-mkMaxIntensity([direc filesep imglist(ii).name],['MaxIntensity' filesep 'MAX' imglist(ii).name])
+    file = [direc filesep imglist(ii).name];
+    [~,name,~] = fileparts(file);
+mkMaxIntensity(file,[direc filesep 'MaxIntensity' filesep 'MAX' name '.tif'])
 disp(['MaxIntensity' filesep 'MAX' imglist(ii).name ' complete']);
  end
 disp('All Projections complete');
