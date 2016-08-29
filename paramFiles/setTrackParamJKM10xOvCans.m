c05a973aad3574bf8392ee2dacb884b377dea43c
Joseph Massey <jm48@rice.edu>
2016-08-29 12:57:06 -0500

    added a couple parameter files, and a template script

---
+++ paramFiles/setTrackParamJKM10xOvCans.m
@@ -0,0 +1,14 @@
+function setTrackParamJKM40xOvCans
+
+global userParam;
+
+userParam.L = 25;
+userParam.sizeImg = [1024, 1024];
+
+userParam.verboseCellTrackerEDS = 0;
+userParam.minTrajLen = 5;%4
+userParam.mergeGap = 2;%2 6
+userParam.sclDstCost = [1 2];
+userParam.minlength = 20;
+userParam.mincyto = 0;
+userParam.splineparam = 0.9;
