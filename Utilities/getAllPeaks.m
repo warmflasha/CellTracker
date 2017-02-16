a68a029cb0810f49ca5a05893a7eefb03f317b63
Joseph Massey <jm48@rice.edu>
2017-02-16 17:20:12 -0600

    expanded getAllPeaks to run on fixed data

--- Utilities/getAllPeaks.m
+++ Utilities/getAllPeaks.m
@@ -2,8 +2,25 @@ function [ allPeaks ] = getAllPeaks
 %getAllPeaks reads peaks array in outfiles from a directory defined in analysisParam 
 %            into allPeaks, an mxn cell where m is positions and n is conditions
 %   
+%%
 global analysisParam;
- allPeaks = cell(size(analysisParam.positionConditions,1),size(analysisParam.positionConditions,2));
+allPeaks = cell(size(analysisParam.positionConditions,1),size(analysisParam.positionConditions,2));
+if isfield(analysisParam,'outDirecStyle') && analysisParam.outDirecStyle == 2
+    
+    
+    for iCon = 1:analysisParam.nCon
+    filenames = dir(fullfile(analysisParam.outDirec, ['Out__' analysisParam.conPrefix{iCon} '*.mat'])); %get outfiles    
+        for iPos = 1:length(filenames);
+        load([analysisParam.outDirec filesep filenames(iPos).name]); 
+    allPeaks{iCon,iPos} = peaks;
+    clear('peaks');
+        end
+    end
+end   
+    
+    
+if  analysisParam.outDirecStyle == 1 || ~isfield(analysisParam,'outDirecStyle')
+ 
 for iPos = 0:analysisParam.nPos-1;
    load([analysisParam.outDirec filesep 'pos' int2str(analysisParam.positionConditions(iPos+1)) '.mat'],'peaks');
    allPeaks{iPos+1} = peaks;
@@ -14,4 +31,5 @@ end
 plotX = (0:length(allPeaks{1})-2)*analysisParam.nMinutesPerFrame./60;
 analysisParam.plotX = plotX-analysisParam.tLigandAdded;
 end
-
+%%
+end
