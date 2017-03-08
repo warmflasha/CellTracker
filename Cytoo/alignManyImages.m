e06015f0bb4a2b9e86782b3934838f286061ba3a
Aryeh <warmflasha@gmail.com>
2017-03-08 17:27:57 -0600

    alignManyImages.m function
    uses the fourier alignment. should be standard now. write wrappers to
    this.

---
+++ Cytoo/alignManyImages.m
@@ -0,0 +1,54 @@
+function [acoords, fi] = alignManyImages(imgs,maxov)
+
+dims = size(imgs);
+tot_imgs=dims(1)*dims(2);
+% zz=zeros(tot_imgs,2);
+% zz=mat2cell(zz,ones(tot_imgs,1));
+% 
+% acoords=struct('wabove',zz,'wside',zz,'absinds',zz);
+
+for ii=1:dims(1)
+    for jj=1:dims(2)
+        currimgind=sub2ind(dims,ii,jj);
+        currimg = imgs{ii,jj};
+        if ii > 1 %if not in top row, align with above
+            previmg = imgs{ii-1,jj};
+            [~ , ind, ind2]=alignTwoImagesFourier(previmg,currimg,1,maxov);
+            acoords(ii,jj).wabove=[ind ind2];
+        else
+            acoords(ii,jj).wabove=[0 0];
+        end
+        if jj > 1 %align with left
+            leftimg=imgs{ii,jj-1};
+            [fi, ind, ind2]=alignTwoImagesFourier(leftimg,currimg,4,maxov);
+            acoords(ii,jj).wside=[ind2 ind];
+        else
+            acoords(ii,jj).wside=[0 0];
+        end
+    end
+end
+
+
+si=size(imgs{1,1});
+if nargout == 2
+    fullIm=zeros(si(1)*dims(1),si(2)*dims(2));
+end
+
+for jj=1:dims(2)
+    for ii=1:dims(1)
+            currinds=[(ii-1)*si(1)+1 (jj-1)*si(2)+1];
+            for kk=2:ii
+                currinds(1)=currinds(1)-acoords(kk,jj).wabove(1);
+            end
+            for mm=2:jj
+                currinds(2)=currinds(2)-acoords(ii,mm).wside(1);
+            end
+            acoords(ii,jj).absinds=currinds;
+            if nargout == 2
+                currimg=imgs{ii,jj};
+                fi(currinds(1):(currinds(1)+si(1)-1),currinds(2):(currinds(2)+si(2)-1))=currimg;
+            end
+    end
+end
+
+
