c346a1c7a1a9f4b4f6514750ae1b6d8e556fe53c
anemashkalo <nmnemashkalo@gmail.com>
2018-01-23 15:41:12 -0700

    function to extract trackf from ilastik autotracking (beta version 1.3); needed for GUI too

--- AN_FinalDataAnalysisFunctions/sortingAnalysisfunctions/process_ilastik_trackAN.m
+++ AN_FinalDataAnalysisFunctions/sortingAnalysisfunctions/process_ilastik_trackAN.m
@@ -4,7 +4,7 @@ function [coordintime] = process_ilastik_trackAN(il_tracks)
 % cannot get the CSV table, only could get the .h5 , each image has the
 % cell already labeled with unique trackID
 trackout_h5 = h5read(il_tracks,'/exported_data');
-h5disp('SortingGFPS4cellspluri70to30_MIP_80tpts_testDynamics_MIP_f0000_w0000_Tracking-Result.h5','/exported_data');
+%h5disp('SortingGFPS4cellspluri70to30_MIP_80tpts_testDynamics_MIP_f0000_w0000_Tracking-Result.h5','/exported_data');
 track_mask = squeeze(trackout_h5); % labeled masks, each object has the same pxl value in time
 %track_mask2 = uint16(track_mask);%
 %at each time point the number of objects will be different, but the same
@@ -29,7 +29,7 @@ for ii=1:nT
     end
     end
 end
-coordintime(objID).dat
+coordintime(objID).dat;
 % TODO: how to extract divisions and merged objects from the h5 file
 end
 
